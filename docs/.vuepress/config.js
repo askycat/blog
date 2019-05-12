56632d012a563e2f6b8c1ca79674ea35d0584180
askycat <875387716@qq.com>
2019-05-12 19:31:17 +0800

    init blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    base: '/',
+    title: '金光布袋戏',
+    description: ' ',
+    themeConfig: {
+        search: false
+      }
+    }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,4 @@
-# Hello VuePress! 
+---
+home: true
+heroImage: /BW.jpg
+---
