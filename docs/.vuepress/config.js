442ef7518c2eb7d84c1dc4b59b5ca63b2632e8c9
askycat <875387716@qq.com>
2019-05-30 18:40:24 +0800

    categories init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,24 @@
 module.exports = {
-    title: 'GO SKY',
-    description: ' ',
-    themeConfig: {
-        search: false
+  title: '筆記',
+  description: '瞭解筆記的創建並維護',
+  serviceWorker: true,
+  themeConfig: {
+    nav: [
+      {
+        text: 'START',
+        link: '/vue/vuepress/',
+      },
+      {
+        text: '.NET CORE',
+        link: '/NETCORE/',
+      },
+      {
+        text: 'VUE',
+        items: [
+          { text: 'VuePress', link: '/VUE/VuePress/' },
+          { text: 'Vuetify', link: '/VUE/Vuetify/' },
+        ]
       }
-    }
+    ]
+  }
+}
